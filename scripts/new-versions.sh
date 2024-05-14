c9625b9d44155f45c188527e28eef19fa4f9e16b
Dan Pock <self@danpock.me>
2024-05-14 15:03:11 -0400

    wip base image build

---
+++ scripts/new-versions.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+: "${IMAGE_REPO:=rancher/kubectl}"
+: "${EXISTING_VERSIONS:=''}"
+
+while IFS= read -r RELEASE_VERSION; do
+  echo "Checking if image ${IMAGE_REPO}:${RELEASE_VERSION} exists"
+  for RELEASE_VERSION in $EXISTING_VERSIONS; do
+    if [ "$RELEASE_VERSION" == "$RELEASE_VERSION" ]; then
+      echo "Image ${IMAGE_REPO}:${RELEASE_VERSION} already exists, skipping"
+      continue 2
+    fi
+  done
+
+  echo "Image ${IMAGE_REPO}:${RELEASE_VERSION} does not exist, adding to new versions list"
+  echo "${RELEASE_VERSION}" >> new-versions.txt
+done < versions.txt
