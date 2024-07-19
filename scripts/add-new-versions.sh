f55a1f5c448c7b48a76db09f71c31112b9065c60
Dan Pock <self@danpock.me>
2024-07-19 16:32:40 -0400

    minimize the repo down to just "rancher-kuberlr" (eventually rancher/kuberlr)

--- scripts/add-new-versions.sh
+++ scripts/add-new-versions.sh
@@ -2,20 +2,21 @@
 set -euxo pipefail
 RELEASES=$(gh api graphql -F owner='flavio' -F name='kuberlr' -f query='query($name: String!, $owner: String!) {repository(owner: $owner, name: $name) {releases(first: 100) {nodes { tagName, isPrerelease }} }}' | jq -r '.data.repository.releases.nodes[] | select(.isPrerelease != true) | .tagName' | sort -V)
 # Including v0.4.3 and higher
-INCLUDE_VERSIONS="v0\.([4-9]{1}\.[3-9]{1}|[0-9]{2,}\.[0-9]{2,})$"
-VERSIONS_FILE="${1:-versions.txt}"
+INCLUDE_VERSIONS="v0\.(4.([3-9]{1}|[0-9]{2,})|[5-9]{1}\.[0-9]+|[0-9]{2,}\.[0-9]+)$"
+VERSIONS_FILE_PATH="${1:-versions.txt}"
 
 ADDED_VERSIONS=()
 
 for RELEASE in $RELEASES; do
   if [[ $RELEASE =~ $INCLUDE_VERSIONS ]]; then
     echo "Version ${RELEASE} matched include versions, checking if already present"
-    if ! grep -q "^${RELEASE}$" "${VERSIONS_FILE}"; then
-      echo "Version ${RELEASE} not present in versions file ${VERSIONS_FILE}, adding"
+    if ! grep -q "^${RELEASE}$" "${VERSIONS_FILE_PATH}"; then
+      echo "Version ${RELEASE} not present in versions file ${VERSIONS_FILE_PATH}, adding"
       ADDED_VERSIONS+=( "${RELEASE}" )
-      echo "${RELEASE}" >> "${VERSIONS_FILE}"
+      echo >> "${VERSIONS_FILE_PATH}"
+      echo "${RELEASE}" >> "${VERSIONS_FILE_PATH}"
     else
-      echo "Version ${RELEASE} already present in versions file ${VERSIONS_FILE}, skipping"
+      echo "Version ${RELEASE} already present in versions file ${VERSIONS_FILE_PATH}, skipping"
     fi
   else
     echo "Version ${RELEASE} does not match include versions, skipping"
@@ -24,7 +25,9 @@ done
 
 if [ "${#ADDED_VERSIONS[@]}" -gt 0 ]; then
   echo "Added [${#ADDED_VERSIONS[@]}] versions: (${ADDED_VERSIONS[*]})"
-  sort -rV -o "${VERSIONS_FILE}" "${VERSIONS_FILE}"
+  cp "${VERSIONS_FILE_PATH}" ./versions.txt
+  awk NF < ./versions.txt | sort -rV -o "${VERSIONS_FILE_PATH}"
+  rm ./versions.txt
 else
   echo "No new versions added"
 fi
