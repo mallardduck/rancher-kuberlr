14c137e0d0c55a9643270bf0b3901fdc4a07e019
Dan Pock <self@danpock.me>
2024-07-19 16:44:28 -0400

    more cleanup fixes/refactors

--- scripts/new-kuberlr-versions.sh
+++ scripts/new-kuberlr-versions.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-: "${IMAGE_REPO:=rancher/kubectl}"
+: "${IMAGE_REPO:=rancher/kuberlr}"
 : "${EXISTING_VERSIONS:=''}"
+: "${VERSIONS_FILE_PATH:=package/versions.txt}"
 
 while IFS= read -r RELEASE_VERSION; do
   CHECK_VERSION="base-${RELEASE_VERSION}"
@@ -16,4 +17,4 @@ while IFS= read -r RELEASE_VERSION; do
 
   echo "Image ${IMAGE_REPO}:${CHECK_VERSION} does not exist, adding to new versions list"
   echo "${RELEASE_VERSION}" >> new-versions.txt
-done < base/versions.txt
+done < "$VERSIONS_FILE_PATH"
